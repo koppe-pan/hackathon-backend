cd469eb30b7d23191cc6f7a7e89f59f4cf323672
koppe_pan <takas3141@gmail.com>
2020-10-12 09:12:12 +0900

    seeds データの追加

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,8 +13,45 @@ Backend.Repo.insert!(%Backend.Companies.Company{
   id: 1
 })
 
+Backend.Repo.insert!(%Backend.Companies.Company{
+  id: 2
+})
 Backend.Repo.insert!(%Backend.Users.User{
   company_id: 1,
   name: "sample_user",
   role: "president"
+})
+
+Backend.Repo.insert!(%Backend.Users.User{
+  company_id: 1,
+  name: "sample_user_2",
+  role: "director"
+})
+
+Backend.Repo.insert!(%Backend.Users.User{
+  company_id: 2,
+  name: "sample_user_3",
+  role: "president"
+})
+Backend.Repo.insert!(%Backend.HealthDatas.HealthData{
+  comment: "health data for sample_user",
+  step: 10,
+  user_id: 1
+})
+Backend.Repo.insert!(%Backend.HealthDatas.HealthData{
+  comment: "health data for sample_user_2",
+  date: ~D[2000-01-01],
+  step: 5,
+  user_id: 2
+})
+Backend.Repo.insert!(%Backend.HealthDatas.HealthData{
+  comment: "health data for sample_user_3",
+  date: ~D[2000-01-01],
+  step: 100,
+  user_id: 3
+})
+Backend.Repo.insert!(%Backend.Coupons.Coupon{
+  cost: 1000,
+  description: "sample coupon",
+  life_time: ~N[2000-01-01 23:00:07]
 })
